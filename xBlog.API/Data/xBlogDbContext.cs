5965c1c0eebf519fe957187b650bbc5015a125e7
zaminalili <zaminalili29@gmail.com>
2024-08-14 16:46:27 +0400

    Create DbContext

---
+++ xBlog.API/Data/xBlogDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using xBlog.API.Models.Domains;
+
+namespace xBlog.API.Data
+{
+    public class xBlogDbContext: DbContext
+    {
+        public xBlogDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
